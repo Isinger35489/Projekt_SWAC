dccecdd5959acde5bcac07c35ae93e95d6286d32
Ea94 <is241308@fhstp.ac.at>
2025-11-15 10:39:06 +0100

    raki änderung (login angepasst auf rolentyp, user kann nur ansehen, admin kann alles andere - weiterleitung zum loginmenu)

---
+++ SIMS.Core/LoginState.cs
@@ -0,0 +1,9 @@
+﻿using SIMS.Core;
+
+public class LoginState
+{
+    public string Username { get; set; }
+    public RoleType Role { get; set; }
+    public bool IsLoggedIn => !string.IsNullOrEmpty(Username);
+}
+
